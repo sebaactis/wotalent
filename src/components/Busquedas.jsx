0e2feb0ffb5dad9c5390fe4ae2cd5e8abcf571d8
sebaactis <sebaactis@gmail.com>
2023-06-07 15:24:42 -0300

    busquedas v1

--- src/components/Busquedas.jsx
+++ src/components/Busquedas.jsx
@@ -1,8 +1,70 @@
 import React from 'react'
+import Card from 'react-bootstrap/Card';
 
 const Busquedas = () => {
+
+  const busquedas = [
+    {
+      title: "Empleado Administrativo",
+      description: "Empleado administrativo de 9 a 18hs para empresa importante de seguros"
+    },
+    {
+      title: "Empleado Administrativo",
+      description: "Empleado administrativo de 9 a 18hs para empresa importante de seguros"
+    },
+    {
+      title: "Empleado Administrativo",
+      description: "Empleado administrativo de 9 a 18hs para empresa importante de seguros"
+    },
+    {
+      title: "Empleado Administrativo",
+      description: "Empleado administrativo de 9 a 18hs para empresa importante de seguros"
+    },
+    {
+      title: "Empleado Administrativo",
+      description: "Empleado administrativo de 9 a 18hs para empresa importante de seguros"
+    },
+    {
+      title: "Empleado Administrativo",
+      description: "Empleado administrativo de 9 a 18hs para empresa importante de seguros"
+    },
+    {
+      title: "Empleado Administrativo",
+      description: "Empleado administrativo de 9 a 18hs para empresa importante de seguros"
+    },
+    {
+      title: "Empleado Administrativo",
+      description: "Empleado administrativo de 9 a 18hs para empresa importante de seguros"
+    },
+    {
+      title: "Empleado Administrativo",
+      description: "Empleado administrativo de 9 a 18hs para empresa importante de seguros"
+    }
+
+
+  ];
+
   return (
-    <div>Busquedas</div>
+    <section className="busquedasSection container">
+      <h1 className="fw-bold titleBusquedas my-3">Busquedas</h1>
+      <div className="line"></div>
+
+      <div className="busquedasItems">
+        {busquedas.map((busqueda) => {
+          return (
+            <Card className="cardBusqueda" key={busqueda.title}>
+              <Card.Body className="cardBody">
+                <Card.Title className="cardTitle">{busqueda.title}</Card.Title>
+                <Card.Text>
+                  {busqueda.description}
+                </Card.Text>
+                <button className="buttonCard">Postularse!</button>
+              </Card.Body>
+            </Card>
+          )
+        })}
+      </div>
+    </section>
   )
 }
 
--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -5,34 +5,36 @@ import { AiOutlineMail } from 'react-icons/ai'
 
 const Footer = () => {
   return (
-    <footer className="footer row">
+    <footer className="footer">
 
-      <section className="mailSection col-lg-4 gap-3 d-flex flex-column justify-content-center">
-        <article className="d-flex gap-3 justify-content-center align-items-start">
-          <AiOutlineMail className="fs-5 mailIcon" />
-          <h3 className="fs-6">
-            Contacto: <span className="m-2 fw-bold"> info@cowotalent.com.ar </span>
-          </h3>
+      <section className="row">
+        <section className="mailSection col-lg-4 gap-3 d-flex flex-column justify-content-center">
+          <article className="d-flex gap-3 justify-content-center align-items-start">
+            <AiOutlineMail className="fs-5 mailIcon" />
+            <h3 className="fs-6">
+              Contacto: <span className="m-2 fw-bold"> info@cowotalent.com.ar </span>
+            </h3>
 
-        </article>
+          </article>
 
-        <article className="d-flex gap-3 justify-content-center align-items-start">
-          <AiOutlineMail className="fs-5 mailIcon"/>
-          <h3 className="fs-6">
-            Envia tu CV:<span className="m-2 fw-bold"> seleccion@cowotalent.com.ar </span> 
-          </h3>
+          <article className="d-flex gap-3 justify-content-center align-items-start">
+            <AiOutlineMail className="fs-5 mailIcon" />
+            <h3 className="fs-6">
+              Envia tu CV:<span className="m-2 fw-bold"> seleccion@cowotalent.com.ar </span>
+            </h3>
 
-        </article>
+          </article>
 
-      </section>
+        </section>
 
-      <section className="navbarFooter col-lg-4 d-flex justify-content-center">
-        <NavBarItems isInFooter={true} />
-      </section>
+        <section className="navbarFooter col-lg-4 d-flex justify-content-center">
+          <NavBarItems isInFooter={true} />
+        </section>
 
-      <section className="logoSection d-flex flex-column justify-content-center align-items-center col-lg-4">
-      <img className="logo" src={logo} alt='logo' />
-        <h3 className="fs-6 text-white fw-bold"> © Todos los derechos reservados COWO Talent 2023</h3>
+        <section className="logoSection d-flex flex-column justify-content-center align-items-center col-lg-4">
+          <img className="logo" src={logo} alt='logo' />
+          <h3 className="fs-6 text-white fw-bold"> © Todos los derechos reservados COWO Talent 2023</h3>
+        </section>
       </section>
     </footer>
   )

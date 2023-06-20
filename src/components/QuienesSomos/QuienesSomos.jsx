22d20b6d583cf0b596cad5c8d25e336f33bce3c8
sebaactis <sebaactis@gmail.com>
2023-06-20 11:57:08 -0300

    framer motion animaciones servicios y quienes somos

--- src/components/QuienesSomos/QuienesSomos.jsx
+++ src/components/QuienesSomos/QuienesSomos.jsx
@@ -1,45 +1,82 @@
-import React from 'react'
+import { motion, useAnimation } from 'framer-motion';
+import React, { useEffect } from 'react'
 import { BiRightArrow } from 'react-icons/bi'
 
 const QuienesSomos = () => {
+
+  const animateParagraphs = () => {
+    const controls = useAnimation();
+
+    useEffect(() => {
+      controls.start(() => ({
+        x: 0,
+        opacity: 1,
+        transition: { delay: 0.2, duration: 0.4 }
+      }));
+    }, [controls]);
+
+    return controls;
+  };
+
+  const controls = animateParagraphs();
+
+
   return (
+
+
     <section className="quienesSomosMain container">
       <h1 className="fw-bold titleQuienes my-3">¿Quiénes Somos?</h1>
       <div className="line"></div>
       <div className="paragraphs my-2 d-flex flex-column gap-4">
 
-        <article className="fw-bold fs-5 d-flex gap-3">
+        <motion.article
+          initial={{ x: -100, opacity: 0 }}
+          animate={controls}
+          className="fw-bold fs-5 d-flex gap-3">
           <span className="arrowText"> <BiRightArrow /></span>
           <p>
             Somos una consultora de Recursos Humanos conformada por un equipo de profesionales altamente capacitados en Gestión de <span className="wordQuienes">Recursos Humanos</span> y <span className="wordQuienes">Psicología.</span>
           </p>
 
-        </article>
-        <article className="fw-bold fs-5 d-flex gap-3">
+        </motion.article>
+        <motion.article
+          initial={{ x: -100, opacity: 0 }}
+          animate={controls}
+          className="fw-bold fs-5 d-flex gap-3">
           <span className="arrowText"> <BiRightArrow /></span>
           <p>
             Nos especializamos en la selección de personal para todo tipo de perfiles, trabajando con diversas organizaciones. Nuestro objetivo principal es satisfacer las necesidades de nuestros clientes y establecer relaciones a largo plazo.
           </p>
-        </article>
-        <article className="fw-bold fs-5 d-flex gap-3">
+        </motion.article>
+        <motion.article
+          initial={{ x: -100, opacity: 0 }}
+          animate={controls}
+          className="fw-bold fs-5 d-flex gap-3">
           <span className="arrowText"> <BiRightArrow /></span>
-          <p>
+          <p initial={{ opacity: 0 }}
+            animate={controls}>
             Adoptamos un enfoque personalizado y proactivo, comprometiéndonos a encontrar a los mejores candidatos para cada posición.
           </p>
-        </article>
-        <article className="fw-bold fs-5 d-flex gap-3">
+        </motion.article>
+        <motion.article
+          initial={{ x: -100, opacity: 0 }}
+          animate={controls}
+          className="fw-bold fs-5 d-flex gap-3">
           <span className="arrowText"> <BiRightArrow /></span>
           <p>
             Nos apasiona ser el puente entre tu empresa y los candidatos ideales.
           </p>
-        </article>
-        <article className="fw-bold fs-5 d-flex gap-3">
+        </motion.article>
+        <motion.article
+          initial={{ x: -100, opacity: 0 }}
+          animate={controls}
+          className="fw-bold fs-5 d-flex gap-3">
           <span className="arrowText"> <BiRightArrow /></span>
           <p>
 
             Nuestra <span className="wordQuienes">honestidad</span>, <span className="wordQuienes">agilidad</span> y  <span className="wordQuienes">transparencia</span>  son nuestras principales características distintivas en el proceso de reclutamiento.
           </p>
-        </article>
+        </motion.article>
 
       </div>
 
--- src/components/Servicios/Servicios.jsx
+++ src/components/Servicios/Servicios.jsx
@@ -1,10 +1,29 @@
-import React from 'react'
+import React, { useEffect } from 'react'
 import { RiUserSearchLine } from 'react-icons/ri'
 import { BsFileEarmarkMedical } from 'react-icons/bs'
 import { BiRightArrow } from 'react-icons/bi'
+import { useAnimation, motion } from 'framer-motion'
 
 
 const Servicios = () => {
+
+
+  const animateParagraphs = () => {
+    const controls = useAnimation();
+
+    useEffect(() => {
+      controls.start(() => ({
+        x: 0,
+        opacity: 1,
+        transition: { delay: 0.2, duration: 0.4 }
+      }));
+    }, [controls]);
+
+    return controls;
+  };
+
+  const controls = animateParagraphs();
+
   return (
     <section className="serviciosSection container">
       <h1 className="fw-bold titleServicios my-3">Servicios</h1>
@@ -20,30 +39,42 @@ const Servicios = () => {
           </div>
 
           <div className="selectionDiv fs-5 d-flex flex-column gap-3 mx-2">
-            <div className="d-flex gap-3">
+            <motion.div
+              initial={{ x: -100, opacity: 0 }}
+              animate={controls}
+              className="d-flex gap-3">
               <span > <BiRightArrow /></span>
               <p>
                 En <span className="fw-bold selectionWord">WO Talent </span>brindamos el servicio de búsqueda y selección de personal para todo tipo de perfiles.
               </p>
-            </div>
-            <div className="d-flex gap-3">
+            </motion.div>
+            <motion.div
+              initial={{ x: -100, opacity: 0 }}
+              animate={controls}
+              className="d-flex gap-3">
               <span > <BiRightArrow /></span>
               <p>
                 Nuestro objetivo es encontrar al candidato perfecto que se ajuste tanto al puesto solicitado por el cliente (pasarlo a vos) como a la cultura de tu empresa, para lograr resultados exitosos.
               </p>
-            </div>
-            <div className="d-flex gap-3">
+            </motion.div>
+            <motion.div
+              initial={{ x: -100, opacity: 0 }}
+              animate={controls}
+              className="d-flex gap-3">
               <span > <BiRightArrow /></span>
               <p>
                 Nuestro equipo de expertos se especializa en diferentes áreas para comprender las necesidades específicas de cada cliente.
               </p>
-            </div>
-            <div className="d-flex gap-3">
+            </motion.div>
+            <motion.div
+              initial={{ x: -100, opacity: 0 }}
+              animate={controls}
+              className="d-flex gap-3">
               <span > <BiRightArrow /></span>
               <p>Nos enfocamos en encontrar los mejores candidatos de manera rápida y eficiente, utilizando métodos efectivos de selección. Estamos comprometidos en encontrar el talento que tu empresa necesita para crecer y prosperar.
 
               </p>
-            </div>
+            </motion.div>
 
           </div>
 
@@ -56,11 +87,15 @@ const Servicios = () => {
             </h4>
           </div>
 
-          <p className="fs-5 py-4">Proximamente...</p>
+          <motion.p initial={{ x: -100, opacity: 0 }}
+            animate={controls}
+            className="fs-5 py-4">
+            Proximamente...
+          </motion.p>
 
         </article>
 
-        
+
       </div>
 
     </section>

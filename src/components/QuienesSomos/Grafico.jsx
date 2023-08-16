ddf0043fec0b7406a7a1b2f14ea1f622dceb840f
sebaactis <sebaactis@gmail.com>
2023-08-16 18:04:11 -0300

    iconos nuevos

--- src/components/QuienesSomos/Grafico.jsx
+++ src/components/QuienesSomos/Grafico.jsx
@@ -2,7 +2,7 @@
 /* eslint-disable react/prop-types */
 import { motion } from 'framer-motion'
 
-const Grafico = ({ icono, titulo, descripcion }) => {
+const Grafico = ({ icono, titulo, descripcion, tamaño }) => {
   return (
     <motion.article className="graficoArticle"
     initial={{ opacity: 0, y: -100 }}
@@ -10,8 +10,8 @@ const Grafico = ({ icono, titulo, descripcion }) => {
     transition={{ delay: 0.5, duration: 0.3 }}
     >
 
-        <img className="iconoGrafico" src={icono} />
-        <h4 className="tituloGrafico">{titulo}</h4>
+        <img src={icono} width={tamaño} />
+        <h4 style={{ textAlign: 'center', fontWeight: 'bold', maxWidth: '80%', marginTop: '0.2rem' }} >{titulo}</h4>
         <p className="descGrafico">{descripcion}</p>
 
     </motion.article>
--- src/components/QuienesSomos/QuienesSomos.jsx
+++ src/components/QuienesSomos/QuienesSomos.jsx
@@ -1,5 +1,7 @@
 /* eslint-disable react/react-in-jsx-scope */
 import icono from '../../assets/images/icono.png'
+import icono2 from '../../assets/images/icono2.png'
+import icono3 from '../../assets/images/icono3.png'
 
 import Grafico from './Grafico'
 
@@ -11,9 +13,9 @@ const QuienesSomos = () => {
       <div className="line"></div>
 
       <div className="graficosQuienesSomos">
-        <Grafico icono={icono} titulo={'Expertos en Recursos Humanos y Psicología'} descripcion={'Somos una consultora de Recursos Humanos conformada por un equipo de profesionales altamente capacitados en Gestión de Recursos Humanos y Psicología.'} />
-        <Grafico icono={icono} titulo={'Selección personalizada y orientada a resultados'} descripcion={'Nos especializamos en la selección de personal para distintos tipos de perfiles. Adoptamos un enfoque personalizado y proactivo, comprometiéndonos a encontrar a los mejores candidatos para cada posición.'} />
-        <Grafico icono={icono} titulo={'Compromiso, agilidad y pasión'} descripcion={'Nos destacamos por ser el puente entre tu empresa y los candidatos ideales. Estas características nos definen en el proceso de principio a fin a la hora de reclutar.'} />
+        <Grafico tamaño={'110px'} icono={icono} titulo={'Expertos en Recursos Humanos y Psicología'} descripcion={'Somos una consultora de Recursos Humanos conformada por un equipo de profesionales altamente capacitados en Gestión de Recursos Humanos y Psicología.'} />
+        <Grafico tamaño={'117px'} icono={icono2} titulo={'Selección personalizada y orientada a resultados'} descripcion={'Nos especializamos en la selección de personal para distintos tipos de perfiles. Adoptamos un enfoque personalizado y proactivo, comprometiéndonos a encontrar a los mejores candidatos para cada posición.'} />
+        <Grafico tamaño={'117px'} icono={icono3} titulo={'Compromiso, agilidad y pasión'} descripcion={'Nos destacamos por ser el puente entre tu empresa y los candidatos ideales. Estas características nos definen en el proceso de principio a fin a la hora de reclutar.'} />
       </div>
 
     </section>

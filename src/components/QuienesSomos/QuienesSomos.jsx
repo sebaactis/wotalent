a76a5b44daf1986b0817c20dbf9fac324b5772e8
Sebastian Actis <Sebastian.Actis@globalprocessing.com.ar>
2023-08-16 18:46:15 -0300

    icono1

--- src/components/QuienesSomos/QuienesSomos.jsx
+++ src/components/QuienesSomos/QuienesSomos.jsx
@@ -13,7 +13,7 @@ const QuienesSomos = () => {
       <div className="line"></div>
 
       <div className="graficosQuienesSomos">
-        <Grafico width={'125px'} padding={0} icono={icono2} titulo={'Expertos en Recursos Humanos y Psicología'} descripcion={'Somos una consultora de Recursos Humanos conformada por un equipo de profesionales altamente capacitados en Gestión de Recursos Humanos y Psicología.'} />
+        <Grafico width={'80px'} margin={1.2} padding={1.48} icono={icono2} titulo={'Expertos en Recursos Humanos y Psicología'} descripcion={'Somos una consultora de Recursos Humanos conformada por un equipo de profesionales altamente capacitados en Gestión de Recursos Humanos y Psicología.'} />
         <Grafico width={'125px'} padding={0} icono={icono3} titulo={'Selección personalizada y orientada a resultados'} descripcion={'Nos especializamos en la selección de personal para distintos tipos de perfiles. Adoptamos un enfoque personalizado y proactivo, comprometiéndonos a encontrar a los mejores candidatos para cada posición.'} />
         <Grafico width={'90px'} margin={0.9} padding={1.2} icono={icono} titulo={'Compromiso, agilidad y pasión'} descripcion={'Nos destacamos por ser el puente entre tu empresa y los candidatos ideales. Estas características nos definen en el proceso de principio a fin a la hora de reclutar.'} />
       </div>

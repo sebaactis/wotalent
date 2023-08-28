c6c6dd34834463efbbb705f418f1d1314e3eb75c
Sebastian Actis <Sebastian.Actis@globalprocessing.com.ar>
2023-08-28 15:10:56 -0300

    animaciones home y quienes somos useView

--- src/components/Home/Home.jsx
+++ src/components/Home/Home.jsx
@@ -9,8 +9,16 @@ import entrevistaSector from '../../assets/images/entrevistaSector.png'
 import estrategia from '../../assets/images/estrategia.png'
 import preseleccionTalento from '../../assets/images/preseleccionTalento.png'
 import seguimiento from '../../assets/images/seguimiento.png'
+import { useInView } from 'react-intersection-observer'
 
 const Home = () => {
+  const [ref, inView] = useInView({ triggerOnce: true })
+
+  const fadeInAnimation = {
+    hidden: { opacity: 0, y: -100 },
+    visible: { opacity: 1, y: 0 }
+  }
+
   const text2 = 'Buscamos tu candidato ideal'
 
   return (
@@ -40,18 +48,30 @@ const Home = () => {
         </motion.div>
       </div>
       <div className="homeProcess">
-        <div className="firstRow">
+        <motion.div
+        ref={ref}
+        initial='hidden'
+        animate={inView ? 'visible' : 'hidden'}
+        transition={{ delay: 0.5, duration: 0.4 }}
+        variants={fadeInAnimation}
+        className="firstRow">
           <CardHome imagen={definicion} texto={'Definimos el perfil ideal para tu empresa, identificando las habilidades y cualidades necesarias'} />
           <CardHome imagen={estrategia} texto={'Creamos una estrategia de búsqueda personalizada para encontrar a los mejores candidatos que se ajusten a tu perfil'} />
           <CardHome imagen={atraccion} texto={'Atraemos talento de alto nivel utilizando diversas fuentes y técnicas de reclutamiento para atraer a los mejores profesionales'} />
-        </div>
-        <div className="secondRow">
+        </motion.div>
+        <motion.div
+        ref={ref}
+        initial='hidden'
+        animate={inView ? 'visible' : 'hidden'}
+        transition={{ delay: 0.9, duration: 0.4 }}
+        variants={fadeInAnimation}
+        className="secondRow">
           <CardHome imagen={preseleccionTalento} texto={'Realizamos una minuciosa preselección de candidatos, evaluando sus habilidades y experiencia.'} />
           <CardHome imagen={entrevistaSector} texto={'Evaluamos a los candidatos para garantizar su idoneidad y alinearlos con tus necesidades'} />
           <CardHome imagen={entrevistaCliente} texto={'Facilitamos entrevistas entre clientes y candidatos, asegurando una comunicación efectiva.'} />
           <CardHome imagen={seguimiento} texto={'Manteniendo un seguimiento cercano y proporcionando feedback continuo, garantizamos una colaboración exitosa a largo plazo'} />
 
-        </div>
+        </motion.div>
 
       </div>
 
--- src/components/QuienesSomos/Grafico.jsx
+++ src/components/QuienesSomos/Grafico.jsx
@@ -1,13 +1,23 @@
 /* eslint-disable react/react-in-jsx-scope */
 /* eslint-disable react/prop-types */
 import { motion } from 'framer-motion'
+import { useInView } from 'react-intersection-observer'
 
 const Grafico = ({ icono, titulo, descripcion, width, padding, margin }) => {
+  const [ref, inView] = useInView({ triggerOnce: true })
+
+  const fadeInAnimation = {
+    hidden: { opacity: 0, y: -100 },
+    visible: { opacity: 1, y: 0 }
+  }
+
   return (
     <motion.article style={ { paddingTop: `${padding}rem` }} className="graficoArticle"
-    initial={{ opacity: 0, y: -100 }}
-    animate={{ opacity: 1, y: 0 }}
-    transition={{ delay: 0.5, duration: 0.3 }}
+    ref={ref}
+    initial="hidden"
+    animate={inView ? 'visible' : 'hidden'}
+    transition={{ delay: 0.7, duration: 0.3 }}
+    variants={fadeInAnimation}
     >
 
         <img src={icono} width={width} />
